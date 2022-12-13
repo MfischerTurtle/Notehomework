616fa250818e77a3ba006fd4e8f366e570759cb6
KungFuTurtle <m.rfischer5235@gmail.com>
2022-12-13 13:53:42 -0600

    fix port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const api = require('./routes/hubs.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
